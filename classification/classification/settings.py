a5c0d8e046ad0e286ead81da0c2bc52ad86151a7
thbinhh <94433293+thbinhh@users.noreply.github.com>
2022-12-26 15:46:48 +0700

    change port

--- classification/classification/settings.py
+++ classification/classification/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-684hp26#*n9blx&2-$)fm54l3s^znigi9xx-_yqc2i$t1q*v$u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','classificationtweet-production.up.railway.app']
 
 
 # Application definition
